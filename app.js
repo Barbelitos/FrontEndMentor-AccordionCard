ff826ae99014f83ac58074aa793088f071550808
David Fonseca <davidfonseca@davids-mbp.lan>
2021-05-14 10:08:42 +0300

    First Commit

---
+++ app.js
@@ -0,0 +1,14 @@
+let question = document.getElementsByClassName("card_question");
+
+for (let i = 0; i < question.length; i++) {
+    question[i].addEventListener('click', function() {
+        this.classList.toggle('active');
+
+        let answer = this.nextElementSibling;
+        if (answer.style.display === 'block') {
+            answer.style.display = 'none';
+        } else {
+            answer.style.display = 'block';
+        }
+    });
+}
